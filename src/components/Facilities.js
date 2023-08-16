313e9db67aee7c5fe938f5d8bfaa49bac8bf5379
felixobianozie <felixobianozie@gmail.com>
2023-08-16 17:48:06 +0100

    fix: remove line breakers causing backward compatibility warnings during compilation

--- src/components/Facilities.js
+++ src/components/Facilities.js
@@ -11,19 +11,12 @@ import convenience from "../assets/convenience.jpg"
 import research_center from "../assets/resource_center.jpg"
 
 function Facilities(){
-    const abtICT = "We have well equiped ICT laboratories with enough \
-    computers for each student";
-    const abtLibrary = "A standard library with a complete set of resources  \
-    for all our student needs"
-    const abtHostel = "There is a complete, state of the arts hostel accommodation  \
-    for all boarding students"
-    const abtConvenience = "Top rated convenience facilities available to students, staff  \
-    and all our esteemed visitors"
-    const abtResearchCenter = "Research center, with modern equipments for all science  \
-    research and college experiments"
-    const abtStaff = "We pride in the bests, with grand experience  \
-    very agile and dedicated to your child's success"
-
+    const abtICT = "We have well equiped ICT laboratories with enough computers for each student";
+    const abtLibrary = "A standard library with a complete set of resources for all our student needs"
+    const abtHostel = "There is a complete, state of the arts hostel accommodation for all boarding students"
+    const abtConvenience = "Top rated convenience facilities available to students, staff and all our esteemed visitors"
+    const abtResearchCenter = "Research center, with modern equipments for all science research and college experiments"
+    const abtStaff = "We pride in the bests, with grand experience very agile and dedicated to your child's success"
 
     return(
         <div className='w3-containter w3-sand'>
