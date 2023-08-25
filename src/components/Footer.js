27648743f23bee37a3dcfc485ec99825a3d64522
felixobianozie <felixobianozie@gmail.com>
2023-08-26 00:31:51 +0100

    fix: do more work on home page responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
     return (
         <div className="w3-container w3-black w3-row" style={{minHeight:"100px"}}>
             <div className="w3-col s12 l6 w3-padding-16 w3-center">
-            &#169; 2023 LUTHERAN HIGH SCHOOL. All rights reserved.. 
+            &#169; 2023 LUTHERAN HIGH SCHOOL. All rights reserved. 
             </div>
             <div className="w3-col s12 l6" style={{}}>
-                <div className="w3-yellow" style={{marginRight:"auto", marginLeft: "auto", width:"200px"}}>
+                <div style={{marginRight:"auto", marginLeft: "auto", width:"200px"}}>
                     <SocialMediaLinks />
                 </div>
                     
